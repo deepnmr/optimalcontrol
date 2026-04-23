1b5fed702a2dd07b81b3117c417721c485e0b360
Donghan Lee <lee.donghan@gmail.com>
2026-04-23 18:17:08 +0900

    feat: US-056 - Integrate operator core, ROPE, and CROP into public API

--- optimalcontrol/__init__.py
+++ optimalcontrol/__init__.py
@@ -1,12 +1,26 @@
 __version__ = "0.1.0"
 
-# Public API placeholders — implemented in subsequent user stories
-# from optimalcontrol.operators import Ix, Iy, Iz, Ip, Im, E  # TODO: US-004
-# from optimalcontrol.operators import comm  # TODO: US-006
-# from optimalcontrol.operators import liouvillian_comm  # TODO: US-007
-# from optimalcontrol.spin_system import SpinSystem, two_spin_system  # TODO: US-010
-# from optimalcontrol.states import state_from_label, normalise_hs  # TODO: US-014
-# from optimalcontrol.states import fidelity_real  # TODO: US-015
-# from optimalcontrol.rope import rope_g, rope_waveform  # TODO: US-018, US-021
-# from optimalcontrol.crop import crop_eta, crop_waveform, CROPPulse  # TODO: US-024, US-026
+from optimalcontrol.crop import CROPPulse, crop_eta, crop_waveform
+from optimalcontrol.operators import Ix, Iy, Iz, comm, liouvillian_comm
+from optimalcontrol.rope import rope_g, rope_waveform
+from optimalcontrol.spin_system import SpinSystem, two_spin_system
+from optimalcontrol.states import fidelity_real, state_from_label
+
+__all__ = [
+    "__version__",
+    "Ix",
+    "Iy",
+    "Iz",
+    "comm",
+    "liouvillian_comm",
+    "SpinSystem",
+    "two_spin_system",
+    "state_from_label",
+    "fidelity_real",
+    "rope_g",
+    "rope_waveform",
+    "crop_eta",
+    "crop_waveform",
+    "CROPPulse",
+]
 
